2c0fa5fd781298f4e5e91606921bb63e469b05e1
MASTERALD <alexmeraz4567@gmail.com>
2023-04-08 22:36:06 -0600

    Initial commit  08/04/2023

---
+++ app/src/main/java/com/example/pruebamenunotificaciones/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.pruebamenunotificaciones;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Cargar el Fragmento de Notficacion
+        loadFragment(new NotificacionFragment());
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.commit();
+    }
+}
